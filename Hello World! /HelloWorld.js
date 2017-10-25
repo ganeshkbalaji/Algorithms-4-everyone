fc7eeb2acd5b15a3be43a51d2147c22bb8c63a4f
Ganesh KB <ganesh.balaji@elliemae.com>
2017-10-25 16:35:57 -0700

    Added JS HelloWorld file

---
+++ Hello World! /HelloWorld.js
@@ -0,0 +1,7 @@
+//Main function can replace with anyother function name.
+//You can run the below either using node or a local browser.
+function main() {
+  console.log("Hello World!");
+}
+
+main();
